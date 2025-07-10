960540801e32af226e68b3800fa7bcddd3184fed
Joshua Mutiso <joshuamutiso455@gmail.com>
2025-07-10 22:32:15 +0300

    methods

--- src/methods/Calculator.java
+++ src/methods/Calculator.java
@@ -1,18 +1,30 @@
 package methods;
+/*
+Method Description
+findAverage()
+* Calculate the average of three numbers
+* Return the average rounded off to two decimal digits
 
+ */
 class Calculator {
-    public void findAverage(int number1, int number2, int number3) {
-        System.out.println("Calculating final amount to be paid.....");
-        double average =(double) (number1 + number2 + number3) / 3;
-        System.out.format("%.2f", average);
+    public int num1;
+    public int num2;
+    public int num3;
+
+    public double findAverage(int number1, int number2, int number3) {
+        double average = (double)(number1 + number2 + number3) / 3;
+        return Math.round(average * 100.0)/ 100.0;
     }
 }
 class Tester{
     public static void main(String[] args) {
         //Object creation
         Calculator calculator = new Calculator();
-        int num1 = 12, num2 = 8, num3 = 15;
-        calculator.findAverage(num1, num2, num3);
+        calculator.num1 = 12;
+        calculator.num2 = 8;
+        calculator.num3 = 15;
+        double average = calculator.findAverage(calculator.num1, calculator.num2, calculator.num3);
+        System.out.println(average);
     }
 }
 
