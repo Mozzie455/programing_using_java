c8b913cdd6bfb6345d4e7d3e24ba473bd513a3ff
Joshua Mutiso <joshuamutiso455@gmail.com>
2025-07-10 13:58:12 +0300

    string

---
+++ src/string/Tester.java
@@ -0,0 +1,29 @@
+package string;
+
+/*
+Problem Statement:
+Complete the removeWhiteSpaces() method given in the Tester class.
+
+Method Description
+
+removeWhiteSpaces(String str)
+
+Remove all the white spaces from the string passed to the method and return the modified string.
+
+Test the functionalities using the main() method of the Tester class.
+
+
+ */
+public class Tester {
+    public static String removeWhiteSpaces(String str){
+        //Implement your code here and change the return value accordingly
+        return str.replaceAll("\\s","");
+
+    }
+
+    public static void main(String[] args){
+        String str = "Hello   How are you   ";
+        str = removeWhiteSpaces(str);
+        System.out.println(str);
+    }
+}
